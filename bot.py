91a0d92f0b24b320ab6ffb74c2f8b4da24518364
Micah <micahincorporated@gmail.com>
2019-04-10 07:31:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 prefix = "u!"
 bot = commands.Bot(command_prefix='u!')
 await client.change_status(game=discord.Game(name='ah, I see now'))
-bot.run(NTY1NTAzNjk2ODE4MTQzMjUz.XK3ZkA.qBfm0al8bXW-3T7r0GJ2n8DvdhA)
+bot.run('NTY1NTAzNjk2ODE4MTQzMjUz.XK3ZkA.qBfm0al8bXW-3T7r0GJ2n8DvdhA')
 @bot.event
 async def on_ready():
     print("ready")
